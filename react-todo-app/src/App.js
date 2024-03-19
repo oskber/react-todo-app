ca0ca3d3f484b214998e21502688f5d36f6dec25
Oskar Bergman <ocke_bergman@hotmail.com>
2024-03-19 11:58:30 +0100

    react todo app

---
+++ react-todo-app/src/App.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [value, setValue] = useState('');
+  const [items, setItems] = useState([]);
+
+  const completed = items.filter((item) => item.completed == true).length;
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="todo-container">
+          <h1>My ToDo</h1>
+          <p>{`${completed} completed`}</p>
+          <div className="input">
+            <form
+              className="todoForm"
+              onSubmit={(ev) => {
+                ev.preventDefault();
+                const updatedItems = [
+                  ...items,
+                  {
+                    completed: false,
+                    label: value,
+                  },
+                ];
+                setItems(updatedItems);
+                setValue('');
+              }}
+            >
+              <input
+                type="text"
+                id="todoInput"
+                value={value}
+                onChange={(ev) => {
+                  setValue(ev.target.value);
+                }}
+              />
+
+              <button id="addTodo">OK</button>
+            </form>
+          </div>
+
+          <small className=""></small>
+
+          <ul>
+            {items.map((item, index) => {
+              const className = item.completed ? 'completed' : '';
+              return (
+                <li
+                  key={index}
+                  className={className}
+                  onClick={() => {
+                    const updatedItems = items.map((oldItem, oldIndex) => {
+                      return {
+                        completed:
+                          oldIndex === index
+                            ? !oldItem.completed
+                            : oldItem.completed,
+                        label: oldItem.label,
+                      };
+                    });
+                    setItems(updatedItems);
+                  }}
+                >
+                  <span>{item.label}</span>
+                  <span className="trashcan">
+                    <i className="fa-solid fa-trash-can"></i>
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
